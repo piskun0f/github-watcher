f616096abed4ed15eb5907f2549839a8cb946f98
Andrew Piskunov <andrepisk2000@gmail.com>
2020-12-25 22:45:40 +0300

    🎉 Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const nodeExternals = require("webpack-node-externals");
+
+const fronendConfig = {
+    entry: "./src/app/index.tsx",
+    output: {
+        filename: "app.js",
+        path: __dirname + "/dist/app"
+    },
+    mode: "development",
+    plugins: [
+        new htmlWebpackPlugin({template: "./src/app/index.html"})
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[jt]sx?$/,
+                use: {loader: "babel-loader"}
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    }
+}
+
+const backendConfig = {
+    target: "node",
+    entry: "./src/server/index.ts",
+    output: {filename: "server.js"},
+    resolve: {
+        extensions: ['.js', '.ts']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[jt]s$/,
+                use: {loader: "babel-loader"}
+            }
+        ]
+    },
+    externals: [nodeExternals()]
+}
+
+module.exports = [fronendConfig, backendConfig];
