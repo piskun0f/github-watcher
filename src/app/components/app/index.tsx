43b5b3694cf64dcc79b5ad2fd24f18e2776f9b24
Andrew Piskunov <andrepisk2000@gmail.com>
2020-12-26 14:47:13 +0300

    ✨ Create a card

--- src/app/components/app/index.tsx
+++ src/app/components/app/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 
 import Grid from '@material-ui/core/Grid';
 import Fab from '@material-ui/core/Fab';
@@ -14,146 +13,16 @@ export class App extends React.Component {
         return (
             <>
                 <Fab
+                    id='addNewCardBtn'
                     color="primary"
                     aria-label="add"
-                    style={{
-                        margin: 0,
-                        top: 'auto',
-                        left: 'auto',
-                        right: 20,
-                        bottom: 20,
-                        position: 'fixed'
-                    }}
                 >
                     <AddIcon />
                 </Fab>
                 <Grid container justify='center'>
-                    <Grid container spacing={3} style={{ margin: 0, width: '100%' }} xs={12} sm={12} md={10} lg={8} xl={8}>
+                    <Grid id='cards' container item spacing={3} xs={12} sm={12} md={10} lg={8} xl={8}>
                         <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
-                            <InfoCard />
+                            <InfoCard name='React' watchsCount='12k' forksCount='500' starsCount='200k' link='https://github.com/facebook/react' />
                         </Grid>
                     </Grid>
                 </Grid>
--- src/app/components/infocard/index.tsx
+++ src/app/components/infocard/index.tsx
@@ -1,21 +1,56 @@
 import React from 'react';
+
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Chip from '@material-ui/core/Chip';
 
-export default class InfoCard extends React.Component {
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import DeviceHubIcon from '@material-ui/icons/DeviceHub';
+
+import './styles.css';
+
+
+interface InfoCardProps {
+    name: string
+    watchsCount: string
+    starsCount: string
+    forksCount: string
+    link: string
+}
+
+export default class InfoCard extends React.Component<InfoCardProps> {
+    constructor(props: InfoCardProps) {
+        super(props);
+    }
+
     render() : JSX.Element {
         return (
             <Card>
                 <CardContent>
                     <Typography color="textSecondary" variant="h5">
-                        ReactJS
+                        { this.props.name }
                     </Typography>
+                    <div id='info'>
+                        <Chip
+                            icon={<VisibilityIcon />}
+                            label={ this.props.watchsCount }
+                        />
+                        <Chip
+                            icon={<StarBorderIcon />}
+                            label={ this.props.starsCount }
+                        />
+                        <Chip
+                            icon={<DeviceHubIcon />}
+                            label={ this.props.forksCount }
+                        />
+                    </div>
                 </CardContent>
                 <CardActions>
-                    <Button size="small">VIEW ON GITHUB</Button>
+                    <Button size="small" href={ this.props.link }>View on Github</Button>
                 </CardActions>
             </Card>
         );
